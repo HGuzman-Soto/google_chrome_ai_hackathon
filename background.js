7cee8635513b158a7dc65c6ae3bc81fd7272b1ff
Huzaifa <huthaifaalmustafa@gmail.com>
2024-10-19 19:45:46 -0700

    added chrome extension setup files

--- background.js
+++ background.js
@@ -1,14 +1,20 @@
 // background.js
 
+// Create a context menu item for logging highlighted text and page data
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: 'checkForUnderstanding',
     title: 'Check for Understanding',
     contexts: ['selection'], // Only show this option when text is selected
   });
+  chrome.contextMenus.create({
+    id: 'logHighlightedTextAndPageData',
+    title: 'Log Highlighted Text and Page Data',
+    contexts: ['selection'], // Only show this menu when text is selected
+  });
 });
 
-// Handle context menu click
+// Handle the context menu click event
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === 'checkForUnderstanding' && info.selectionText) {
     // Send the selected text to the content script in the active tab
@@ -19,8 +25,21 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     });
   }
 });
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'logHighlightedTextAndPageData' && info.selectionText) {
+    // Send a message to the content script to collect page data
+    chrome.tabs.sendMessage(tab.id, { 
+      type: 'collectData', 
+      highlightedText: info.selectionText // Send the highlighted text with the message
+    }, (response) => {
+      // Combine highlighted text with page data
+      const dataToSend = {
+        highlightedText: info.selectionText,
+        pageData: response.data
+      };
 
-// A function that will log the selected text (invoked by executeScript)
-function logSelectedText(selectedText) {
-  console.log('Selected text:', selectedText);
-}
+      // Log the combined data (or send to an API in the future)
+      console.log('Data to send:', dataToSend);
+    });
+  }
+});
